a0ab927a5988804bc4f2147498b0a3cbf7c101b7
Oscar Armstrong <oscar@otothea.com>
2014-05-29 14:27:01 -0500

    Update readme and config example

--- Readme.md
+++ Readme.md
@@ -10,7 +10,7 @@ config/app.js:
       port:       '8000',
       auth:        true,        // Set to false to disable authentication requirement
       username:   'changeme',
-      password:   'changeme',
+      password:   'changeme',   // Sha512 Hash of your password http://www.xorbin.com/tools/sha256-hash-calculator
       secret:     'CHANGEME',   // Used for session storage
       skype:       true,        // Set to false if no skype installed
     }
--- config/app.example.js
+++ config/app.example.js
@@ -1,9 +1,9 @@
 module.exports = {
   hostname:   'localhost',
   port:       '8000',
-  auth:        true,
+  auth:        true,        // Set to false to disable authentication requirement
   username:   'changeme',
-  password:   'changeme', // Sha512 Hash of your password http://www.xorbin.com/tools/sha256-hash-calculator
-  secret:     'CHANGEME',
-  skype:       true,
+  password:   'changeme',   // Sha512 Hash of your password http://www.xorbin.com/tools/sha256-hash-calculator
+  secret:     'CHANGEME',   // Used for session storage
+  skype:       true,        // Set to false if no skype installed
 };
